01463e6e3091e9669f59bd4e17a02381eec0381f
Andrey Androsov <andrey.sonido@gmail.com>
2022-12-06 23:29:34 +0300

    add UIButton and constrains

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -11,13 +11,28 @@ class ProfileViewController: UIViewController {
 
     let profileHeaderView = ProfileHeaderView()
     
+    private lazy var changeTitleBtn: UIButton = {
+        let changeTitleBtn = UIButton()
+        changeTitleBtn.backgroundColor = .systemBlue
+        changeTitleBtn.setTitle("Change title", for: .normal)
+        changeTitleBtn.tintColor = .white
+        changeTitleBtn.translatesAutoresizingMaskIntoConstraints = false
+        changeTitleBtn.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        return changeTitleBtn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.addSubview(profileHeaderView)
+        self.view.addSubview(changeTitleBtn)
         setup()
     }
-
+    
+    @objc func buttonPressed(sender:UIButton){
+        title = "Изменили тайтл"
+    }
+    
     private func setup() {
         self.view.backgroundColor = .lightGray
         title = "Профиль"
@@ -27,7 +42,11 @@ class ProfileViewController: UIViewController {
             profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0),
             profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            profileHeaderView.heightAnchor.constraint(equalToConstant: 220)
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            changeTitleBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            changeTitleBtn.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            changeTitleBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0),
+            changeTitleBtn.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
