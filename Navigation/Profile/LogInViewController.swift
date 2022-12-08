53c21a048c17a3d23190d43c87d9663f7fce2ca8
Andrey Androsov <andrey.sonido@gmail.com>
2022-12-08 22:56:33 +0300

    Add LogInViewController, logo.png and colorset

---
+++ Navigation/Profile/LogInViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by MacBook on 08.12.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
